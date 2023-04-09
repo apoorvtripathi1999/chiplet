1617d1dac081cdb1033582ed494d335021f278fa
apoorvtripathi1999 <apoorvtripathi1999@gmail.com>
2023-04-09 23:35:25 +0530

    completed description section

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -1,53 +1,95 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import '../CSS/home.css'
+import '../CSS/transitions.css'
+import arduinoDesc from '../Images/home/arduino_desc.png'
+import sensor from '../Images/home/sensor.png'
+import resistor from '../Images/home/resistor.png'
+import connector from '../Images/home/connectors.png'
+
+
 
 export default function Description() {
 
-  const [percent,setPercent] = useState()
-  const [index,setIndex] = useState(0)
-  const arr = ["0%","25%","50%","75%","100%"]
-  let [stateArr,setStatearr] = useState(["active", "inactive","inactive","inactive","inactive"])
- 
+  const [percent, setPercent] = useState("0%")
+  const [index, setIndex] = useState(0)
+  const [btn,setBtn] = useState("hidden")
+  const arr = ["25%", "50%", "75%", "100%"]
+
+
   const progressInterval = setInterval(updateProg, 2000)
 
-  function updateProg () {
-      if(index===5)
-      {
-        clearInterval(progressInterval)
-        setIndex(0)
-        return;
-      }
-      switch (index)
-      {
-         case 0: setStatearr(["active", "inactive","inactive","inactive","inactive"]) ; console.log("changed 1"); break; 
-         case 1: setStatearr(["inactive", "active","inactive","inactive","inactive"]) ; console.log("changed 2"); break;
-         case 2: setStatearr(["inactive", "inactive","active","inactive","inactive"]) ; console.log("changed 3"); break;
-         case 3: setStatearr(["inactive", "inactive","inactive","active","inactive"]) ; console.log("changed 4"); break;
-         case 4: setStatearr(["inactive", "inactive","inactive","inactive","active"]) ; console.log("changed 5"); break;
-         default: return stateArr
-      }
-      const nextProgress = arr[index]
-      setPercent(nextProgress)
-      setIndex(index+1)
-      // console.log(arr[index])
+  function updateProg() {
+
+    const nextProgress = arr[index]
+    setPercent(nextProgress)
+    setIndex(index + 1)
+
+    if (index === 4) {
+      clearInterval(progressInterval)
+      setIndex(0)
+      setPercent("0%")
+      setBtn("visible")
+      return;
+    }
+    console.log(index, percent)
+
   }
 
-  useEffect(()=>
-  {
-    
-  },[percent])
+  useEffect(() => {
+
+  }, [index])
 
 
   return (
-    <div className='description-main'> 
-    <div className='progress-container'>
-      <div className='progress-bar' style={{width:`${percent}`}}></div>
-      <div className='progress-section' id={`${stateArr[0]}`}></div>
-      <div className='progress-section' id={`${stateArr[1]}`}></div>
-      <div className='progress-section' id={`${stateArr[2]}`}></div>
-      <div className='progress-section' id={`${stateArr[3]}`}></div>
-      <div className='progress-section' id={`${stateArr[4]}`}></div>
-    </div>
+    <div className='description-main'>
+
+      <div className='progress-items-container'>
+
+        <div className='progress-item' id={index >= 1 ? "active" : "inactive"}>
+          <div className='description-img-div'>
+            <img src={arduinoDesc} alt='Not Found' className='description-img'></img>
+          </div>
+          <h3 className='desc-text'>Microconrollers and Electronic chips</h3>
+        </div>
+
+        <div className='progress-item' id={index >= 2 ? "active" : "inactive"}>
+          <div className='description-img-div'>
+            <img src={sensor} alt='Not Found' className='description-img'></img>
+          </div>
+          <h3 className='desc-text'><a href='https://www.aiea.in/' target='_blank' rel='noreferrer'>AIEA </a>approved sensors</h3>
+        </div>
+
+        <div className='progress-item' id={index >= 3 ? "active" : "inactive"}>
+          <div className='description-img-div'>
+            <img src={resistor} alt='Not Found' className='description-img'></img>
+          </div>
+          <h3 className='desc-text'>Passive Components</h3>
+        </div>
+
+        <div className='progress-item' id={index > 3 ? "active" : "inactive"}>
+          <div className='description-img-div'>
+            <img src={connector} alt='Not Found' className='description-img'></img>
+          </div>
+          <h3 className='desc-text'>Wirings and Connectors</h3>
+        </div>
+
+      </div>
+
+      <div className='desc-btn-div'>
+        <button className='desc-btn btn fadein' style={{visibility:`${btn}`}}>Check Out Collection</button>
+      </div>
+
+      <div className='progress'>
+      <div className='progress-container'>
+        <div className='progress-bar' style={{ width: `${percent}` }}></div>
+        <div className='progress-section' id={index === 0 ? "active" : "inactive"}></div>
+        <div className='progress-section' id={index === 1 ? "active" : "inactive"}></div>
+        <div className='progress-section' id={index === 2 ? "active" : "inactive"}></div>
+        <div className='progress-section' id={index === 3 ? "active" : "inactive"}></div>
+        <div className='progress-section' id={index === 4 ? "active" : "inactive"}></div>
+      </div>
+      </div>
+
     </div>
   )
 }
